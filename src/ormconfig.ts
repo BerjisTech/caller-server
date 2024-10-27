3bed2438f1f634f7e15dd81de8f4e2523bd3056b
Benedcict <ben@proz.com>
2024-10-27 11:28:58 +0300

    Update entity file paths in ormconfig.ts

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,5 +1,8 @@
 import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
+import { Profile } from './models/Profile';
+import { Room } from './models/Room';
+import { RoomMember } from './models/RoomMember';
 
 dotenv.config();
 
@@ -12,9 +15,7 @@ export const AppDataSource = new DataSource({
     database: process.env.DB_DATABASE,
     synchronize: process.env.DB_SYNCHRONIZE === 'true',
     logging: process.env.DB_LOGGING === 'true',
-    entities: [
-        'src/models/**/*.ts'
-    ],
+    entities: [Room, Profile, RoomMember],
     migrations: [
         'src/migration/**/*.ts'
     ],
